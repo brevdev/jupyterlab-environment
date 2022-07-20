e5e2cb336c96318d5188e85fdebf31abcdaf3359
r33drichards <xxmrmau5@gmail.com>
2022-07-20 23:59:19 +0000

    remove ayar stuff

--- genfuncs.py
+++
@@ -1,8 +0,0 @@
-def format_fn_text(zap, shake):
-    return """biased_{zap}_zap_to_{shake}_shake,""".format(zap=zap, shake=shake)
-
-if __name__ == "__main__":
-    for zap in range(1,100):
-        shake = 100 - zap
-        text = format_fn_text(zap, shake)
-        print(text)
